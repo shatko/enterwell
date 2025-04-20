d08a949357c2cf8810025b0bf06531e9779993b5
Shatko <mladen.salaj@gmail.com>
2025-04-20 20:13:04 +0200

    feat: adds and styles logo

--- web/app/themes/enterwell/resources/views/forms/form.blade.php
+++ web/app/themes/enterwell/resources/views/forms/form.blade.php
@@ -1,5 +1,6 @@
-<div class="form-container">
-  <div class="form-wrapper">
+<div class="form-container flex justify-center mt-8">
+  <div class="form-wrapper max-w-210 border border-solid border-white rounded-lg px-25 pt-10 pb-18 relative">
+    @svg('logo', 'h-33 w-auto absolute -top-8 -left-6.5')
     {!! do_shortcode('[contact-form-7 id="e52367a" title="Contact form 1"]') !!}
   </div>
 </div>
