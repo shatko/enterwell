3788e47ab7c56c584693fd1e0fe0ef7359acb104
Shatko <mladen.salaj@gmail.com>
2025-04-20 19:16:45 +0200

    feat: styles the send button

--- web/app/themes/enterwell/resources/views/forms/form.blade.php
+++ web/app/themes/enterwell/resources/views/forms/form.blade.php
@@ -1,1 +1,5 @@
-{!! do_shortcode('[contact-form-7 id="e52367a" title="Contact form 1"]') !!}
+<div class="form-container">
+  <div class="form-wrapper">
+    {!! do_shortcode('[contact-form-7 id="e52367a" title="Contact form 1"]') !!}
+  </div>
+</div>
