bf07ea4899e0ed4f09cf8634274e42b811c42c29
Shatko <mladen.salaj@gmail.com>
2025-04-20 15:08:52 +0200

    fix: sets form as a partial

---
+++ web/app/themes/enterwell/resources/views/forms/form.blade.php
@@ -0,0 +1,1 @@
+{!! do_shortcode('[contact-form-7 id="e52367a" title="Contact form 1"]') !!}
--- web/app/themes/enterwell/resources/views/template-custom.blade.php
+++ web/app/themes/enterwell/resources/views/template-custom.blade.php
@@ -8,6 +8,7 @@
   @while(have_posts()) @php(the_post())
     @include('partials.content-page')
 
-    {!! do_shortcode('[contact-form-7 id="e52367a" title="Contact form 1"]') !!}
+    @include('forms.form')
+
   @endwhile
 @endsection
