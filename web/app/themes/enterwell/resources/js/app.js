39a6af75f86efb400664d357c340c14239d8af29
Shatko <mladen.salaj@gmail.com>
2025-04-23 10:09:02 +0200

    feat: adds success template

--- web/app/themes/enterwell/resources/js/app.js
+++ web/app/themes/enterwell/resources/js/app.js
@@ -2,3 +2,9 @@ import.meta.glob([
   '../images/**',
   '../fonts/**',
 ]);
+
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+  setTimeout( () => {
+      location = '/success/';
+  }, 1000 );
+}, false );
---
+++ web/app/themes/enterwell/resources/views/template-success.blade.php
@@ -0,0 +1,12 @@
+{{--
+  Template Name: Success Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    @include('partials.content-page')
+
+  @endwhile
+@endsection
